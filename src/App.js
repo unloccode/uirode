be952d1f9e7ec394cd54e6328bc81b73a95726a7
unloccode <unloccode@gmail.com>
2021-06-27 12:30:27 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="text-center">
+            <h3 className="display-2">RODE</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
